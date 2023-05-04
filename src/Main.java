e7903ed3bcd21f9acf7d07ebc42d8fdd3f509803
Emily443 <emilycarolini443@gmail.com>
2023-05-04 12:32:24 -0300

    criada classe pessoa

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        {
-            // Laço enquanto - while
-            int contador = 0;
-            while (contador < 10) {
-                System.out.println("contador = " + contador);
-                //incremento
-                contador = contador + 1;
-            }
-            //laço do para faça-for
-            for (int x = 0; x < 10; x++) {
-                System.out.println("contador = " + x);
-            }
-            //laço fça enquanto = do while
-            int cont = 1;
-            do {
-                //processo
-                System.out.println(cont);
-                cont++; //incremento
-            } while (cont <= 10);
-        }
+        // Aula introdutoria de orientaçao a objetos
+        Pessoa adao = new Pessoa();
     }
 }
